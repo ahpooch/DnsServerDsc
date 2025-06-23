20bde9303496b1d8c971710bc2ee7c3526a5e94c
ahpooch <ahpooch@hotmail.com>
2025-06-23 05:05:49 +0300

    work in progress

--- source/Classes/016.DnsPolicyBase.ps1
+++ source/Classes/016.DnsPolicyBase.ps1
@@ -1,3 +1,4 @@
+WARNING: UNDER CONSTRUCTION!!!!
 <#
     .SYNOPSIS
         A DSC Resource of type Policy for MS DNS Server that is not exposed to end users representing the common fields available to all DNS Policies.
@@ -16,3 +17,229 @@
     .PARAMETER Ensure
         Whether the host record should be present or removed.
 #>
+
+class DnsPolicyBase : ResourcePropertiesBase
+{
+    [DscProperty(Key)]
+    [System.String]
+    $ZoneName
+
+    [DscProperty()]
+    [Ensure]
+    $Ensure = [Ensure]::Present
+
+    # Hidden property for holding localization strings
+    hidden [System.Collections.Hashtable] $localizedData
+
+        # Hidden method to integrate localized strings from classes up the inheritance stack
+    hidden [void] SetLocalizedData()
+    {
+        # Create a list of the inherited class names
+        $inheritedClasses = @(, $this.GetType().Name)
+        $parentClass = $this.GetType().BaseType
+        while ($parentClass -ne [System.Object])
+        {
+            $inheritedClasses += $parentClass.Name
+            $parentClass = $parentClass.BaseType
+        }
+
+        $this.localizedData = @{}
+
+        foreach ($className in $inheritedClasses)
+        {
+
+            try
+            {
+                $tmpData = Get-LocalizedData -DefaultUICulture 'en-US' -FileName $className -ErrorAction Stop
+
+                # Append only previously unspecified keys in the localization data
+                foreach ($key in $tmpData.Keys)
+                {
+                    if (-not $this.localizedData.ContainsKey($key))
+                    {
+                        $this.localizedData[$key] = $tmpData[$key]
+                    }
+                }
+            }
+            catch
+            {
+                if ($_.CategoryInfo.Category.ToString() -eq 'ObjectNotFound')
+                {
+                    Write-Warning $_.Exception.Message
+                }
+                else
+                {
+                    throw $_
+                }
+            }
+        }
+
+        Write-Debug ($this.localizedData | ConvertTo-JSON)
+    }
+
+    # Default constructor loads the localization strings
+    DnsPolicyBase()
+    {
+        # Import the localization strings
+        $this.SetLocalizedData()
+    }
+
+    #region Generic DSC methods -- DO NOT OVERRIDE
+
+    [DnsPolicyBase] Get()
+    {
+        #doubt
+        Write-Verbose -Message ($this.localizedData.GettingDscResourceObject -f $this.GetType().Name)
+        #doubt
+        $dscResourceObject = $null
+        #doubt
+        $record = $this.GetResourceRecord()
+        $doubt
+        if ($null -eq $record)
+        {
+            Write-Verbose -Message $this.localizedData.RecordNotFound
+
+            <#
+                Create an object of the correct type (i.e.: the subclassed resource type)
+                and set its values to those specified in the object, but set Ensure to Absent
+            #>
+            $dscResourceObject = [System.Activator]::CreateInstance($this.GetType())
+
+            foreach ($propertyName in $this.PSObject.Properties.Name)
+            {
+                $dscResourceObject.$propertyName = $this.$propertyName
+            }
+
+            $dscResourceObject.Ensure = 'Absent'
+        }
+        else
+        {
+            Write-Verbose -Message $this.localizedData.RecordFound
+
+            # Build an object reflecting the current state based on the record found
+            $dscResourceObject = $this.NewDscResourceObjectFromRecord($record)
+        }
+
+        return $dscResourceObject
+    }
+
+    [void] Set()
+    {
+        # Initialize dns cmdlet Parameters for removing a record
+        $dnsParameters = @{
+            ZoneName     = $this.ZoneName
+            ComputerName = $this.DnsServer
+        }
+
+        # Accomodate for scoped records as well
+        if ($this.isScoped)
+        {
+            $dnsParameters['ZoneScope'] = ($this.PSObject.Properties | Where-Object -FilterScript { $_.Name -eq 'ZoneScope' }).Value
+        }
+
+        $existingRecord = $this.GetResourceRecord()
+
+        if ($this.Ensure -eq 'Present')
+        {
+            if ($null -ne $existingRecord)
+            {
+                $currentState = $this.Get() | ConvertFrom-DscResourceInstance
+                $desiredState = $this | ConvertFrom-DscResourceInstance
+
+                # Remove properties that have $null as the value
+                @($desiredState.Keys) | ForEach-Object -Process {
+                    if ($null -eq $desiredState[$_])
+                    {
+                        $desiredState.Remove($_)
+                    }
+                }
+
+                # Returns all enforced properties not in desires state, or $null if all enforced properties are in desired state
+                $propertiesNotInDesiredState = Compare-DscParameterState -CurrentValues $currentState -DesiredValues $desiredState -Properties $desiredState.Keys -IncludeValue
+
+                if ($null -ne $propertiesNotInDesiredState)
+                {
+                    Write-Verbose -Message $this.localizedData.ModifyingExistingRecord
+
+                    $this.ModifyResourceRecord($existingRecord, $propertiesNotInDesiredState)
+                }
+            }
+            else
+            {
+                Write-Verbose -Message ($this.localizedData.AddingNewRecord -f $this.GetType().Name)
+
+                # Adding record
+                $this.AddResourceRecord()
+            }
+        }
+        elseif ($this.Ensure -eq 'Absent')
+        {
+            if ($null -ne $existingRecord)
+            {
+                Write-Verbose -Message $this.localizedData.RemovingExistingRecord
+
+                # Removing existing record
+                $existingRecord | Remove-DnsServerResourceRecord @dnsParameters -Force
+            }
+        }
+    }
+
+    [System.Boolean] Test()
+    {
+        $isInDesiredState = $true
+
+        $currentState = $this.Get() | ConvertFrom-DscResourceInstance
+        $desiredState = $this | ConvertFrom-DscResourceInstance
+
+        if ($this.Ensure -eq 'Present')
+        {
+            if ($currentState.Ensure -eq 'Present')
+            {
+                # Remove properties that have $null as the value
+                @($desiredState.Keys) | ForEach-Object -Process {
+                    if ($null -eq $desiredState[$_])
+                    {
+                        $desiredState.Remove($_)
+                    }
+                }
+
+                # Returns all enforced properties not in desires state, or $null if all enforced properties are in desired state
+                $propertiesNotInDesiredState = Compare-DscParameterState -CurrentValues $currentState -DesiredValues $desiredState -Properties $desiredState.Keys -ExcludeProperties @('Ensure')
+
+                if ($propertiesNotInDesiredState)
+                {
+                    $isInDesiredState = $false
+                }
+            }
+            else
+            {
+                Write-Verbose -Message ($this.localizedData.PropertyIsNotInDesiredState -f 'Ensure', $desiredState['Ensure'], $currentState['Ensure'])
+
+                $isInDesiredState = $false
+            }
+        }
+
+        if ($this.Ensure -eq 'Absent')
+        {
+            if ($currentState['Ensure'] -eq 'Present')
+            {
+                Write-Verbose -Message ($this.localizedData.PropertyIsNotInDesiredState -f 'Ensure', $desiredState['Ensure'], $currentState['Ensure'])
+
+                $isInDesiredState = $false
+            }
+        }
+
+        if ($isInDesiredState)
+        {
+            Write-Verbose -Message $this.localizedData.ObjectInDesiredState
+        }
+        else
+        {
+            Write-Verbose -Message $this.localizedData.ObjectNotInDesiredState
+        }
+
+        return $isInDesiredState
+    }
+
+    #endregion
+}
